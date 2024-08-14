a32c5de1d5c50ff541a81ee65c6eb9e9f2e8f2a5
Robert Goss <goss.robert@gmail.com>
2024-08-14 22:20:57 +0100

    Render a sphere

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::fs::File;
 use std::io::{Error, Write};
 use crate::colour::{Colour, write_ppm_colour};
 use crate::ray::Ray;
-use crate::vec3::{Point3, Vec3};
+use crate::vec3::{dot, Point3, Vec3};
 
 fn write_ppm_header(
     file : &mut File,
@@ -26,8 +26,21 @@ fn write_ppm_header(
     Ok(())
 }
 
+fn hit_sphere(center : &Point3, radius : f64, ray : &Ray) -> bool {
+    let oc = center - ray.origin();
+    // Quad formula
+    let a = dot(ray.direction(), ray.direction());
+    let b = -2.0 * dot(ray.direction(), &oc);
+    let c = dot(&oc, &oc) - radius * radius;
+    let discriminant = b*b - 4.0*a*c;
+    discriminant >= 0.0
+}
+
 // Render ray
 fn ray_colour(ray : &Ray) -> Colour {
+    if (hit_sphere(&Point3::new(0.0,0.0, -1.0), 0.5, ray)) {
+        return Colour::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = ray.direction().unit();
     let a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*Colour::new(1.0, 1.0, 1.0) + a*Colour::new(0.5, 0.7, 1.0);
@@ -43,7 +56,7 @@ fn main() {
     );
 
     // Setup the camera coords
-    let focal_length = 2.0;
+    let focal_length = 1.0;
     let viewport_height = 2.0;
     let viewport_width = viewport_height * (image_width as f64)/ (image_height as f64);
     let camera_center = Point3::zero();
@@ -52,7 +65,7 @@ fn main() {
     let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
     // Pixel size in viewport
     let pixel_delta_u = viewport_u / (image_width as f64);
-    let pixel_delta_v = viewport_v / (image_width as f64);
+    let pixel_delta_v = viewport_v / (image_height as f64);
     // Start point
     let viewport_upper_left = camera_center
         - Vec3::new(0.0, 0.0, focal_length) - viewport_u/2.0 - viewport_v/2.0;
